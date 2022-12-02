d7671ac445d0447b624acabb0096a7d1aabdc53b
Oleksandr Lytvyniuk <sandro132015@gmail.com>
2022-12-03 01:36:26 +0200

    t1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const galleryMakeList = galleryItems
+  .map(item => {
+    return `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>    `;
+  })
+  .join('');
+
+gallery.insertAdjacentHTML('afterbegin', galleryMakeList);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
